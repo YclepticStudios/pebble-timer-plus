eeb5cf81ae8294f65e44063584b7a2002058c3e3
Eric Phillips <eric.d.phillips@icloud.com>
2015-10-17 16:50:35 -0400

    Add delay ability to animation framework

--- src/animation.c
+++ src/animation.c
@@ -23,6 +23,7 @@ typedef struct AnimationNode {
   void                    *to;                //< Pointer to value to animate to
   uint64_t                start_time;         //< Millisecond epoch of when animation was started
   uint32_t                duration;           //< Duration of animation in milliseconds
+  uint32_t                delay;              //< Time to wait before animating
   struct AnimationNode    *next;              //< Pointer to next node in linked list
 } AnimationNode;
 
@@ -55,17 +56,21 @@ static int32_t prv_curve_quad_ease_in_out(int32_t from, int32_t to, uint32_t per
 
 // Step a int32 animation
 static void prv_animation_step_int32(AnimationNode *node) {
+  // set from value on first call, allowing another animation to change the target value
+  // while this animation is delayed
+  if (!node->from) {
+    node->from = MALLOC(sizeof(int32_t));
+    (*(int32_t*)node->from) = (*(int32_t*)node->target);
+  }
   // step value
   int32_t from = (*(int32_t*)node->from);
   int32_t to = (*(int32_t*)node->to);
   uint32_t percent_max = node->duration;
-  uint32_t percent = epoch() - node->start_time;
+  uint32_t percent = epoch() - (node->start_time + node->delay);
   (*(int32_t*)node->target) = prv_curve_quad_ease_in_out(from, to, percent, percent_max);
   // continue animation
   if (percent >= percent_max) {
     animation_stop(node->target);
-  } else {
-    prv_animation_timer_start();
   }
 }
 
@@ -88,8 +93,14 @@ static void prv_animation_timer_callback(void *data) {
   // loop over list and step each animation
   AnimationNode *cur_node = head_node;
   while (cur_node) {
-    (*cur_node->step_func)(cur_node);
+    if (epoch() > cur_node->start_time + (uint64_t)cur_node->delay) {
+      (*cur_node->step_func)(cur_node);
+    }
     cur_node = cur_node->next;
+  }
+  // continue animation
+  if (head_node) {
+    prv_animation_timer_start();
   }
   // raise animation update callback
   if (ani_callback) {
@@ -110,26 +121,21 @@ static void prv_animation_timer_start(void) {
 //
 
 // Animate an integer by its pointer
-void animation_int32_start(int32_t *ptr, int32_t to, uint32_t duration) {
+void animation_int32_start(int32_t *ptr, int32_t to, uint32_t duration, uint32_t delay) {
   // create and add new node
   AnimationNode *new_node = (AnimationNode*)MALLOC(sizeof(AnimationNode));
   new_node->step_func = &prv_animation_step_int32;
   new_node->target = ptr;
-  new_node->from = MALLOC(sizeof(int32_t));
+  new_node->from = NULL; // assigned on first "step" callback in case of delayed animation
   new_node->to = MALLOC(sizeof(int32_t));
-  (*(int32_t*)new_node->from) = (*ptr);
   (*(int32_t*)new_node->to) = to;
   new_node->start_time = epoch();
   new_node->duration = duration;
+  new_node->delay = delay;
   new_node->next = NULL;
   prv_list_add_node(new_node);
   // start animation timer if not running
   prv_animation_timer_start();
-}
-
-// Animate a GRect by its pointer
-void animation_grect_start(GRect *ptr, GRect to, uint32_t duration) {
-  // TODO: Develop this function
 }
 
 // Cancel an animation by its pointer
--- src/animation.h
+++ src/animation.h
@@ -15,13 +15,8 @@
 //! @param ptr A pointer to the integer to animate
 //! @param to The value to animate the pointer value to
 //! @param duration The length of time over which to animate the value
-void animation_int32_start(int32_t *ptr, int32_t to, uint32_t duration);
-
-//! Animate a GRect by its pointer
-//! @param ptr A pointer to the GRect to animate
-//! @param to The GRect to animate the pointer value to
-//! @param duration The length of time over which to animate the value
-void animation_grect_start(GRect *ptr, GRect to, uint32_t duration);
+//! @param delay The length of time to wait before running the animation
+void animation_int32_start(int32_t *ptr, int32_t to, uint32_t duration, uint32_t delay);
 
 //! Cancel an animation by its pointer
 //! @param ptr A pointer for which to cancel an animation
